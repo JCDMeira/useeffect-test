a7b21a422f8a84a56e55f536f6b9b762c63b3dc6
Jean Meira <jean.meira10@hotmail.com>
2022-07-17 13:51:03 -0300

    feat: add User component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
+import User from './Components/User';
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [userInfo, _] = useState({
+    name: 'Jean',
+    lastName: 'Meira',
+  });
+
+  const incrementCount = () => setCount((currentNum) => currentNum + 1);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Current count: {count}</h1>
+
+      <button onClick={incrementCount}>Up count</button>
+
+      <User userInfo={userInfo} />
     </div>
   );
 }
